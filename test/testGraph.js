8f6eb6c4aba685289e62fad46f978a3c85bd5abc
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-06-10 20:34:29 +0530

    added tests

--- test/testGraph.js
+++ test/testGraph.js
@@ -10,4 +10,16 @@ describe('bfs', () => {
   it('should give true for the source and the target present in same pair', () => {
     assert.isTrue(bfs([[5, 6]], 5, 6));
   });
+
+  it('should give true for the path found with in two pairs', () => {
+    assert.isTrue(bfs([[5, 6], [6, 7]], 5, 7));
+  });
+
+  it('should give true for the path found with in three pairs', () => {
+    assert.isTrue(bfs([[5, 6], [6, 7], [6, 8]], 5, 8));
+  });
+
+  it('should give false for the path not found', () => {
+    assert.isFalse(bfs([[5, 6], [6, 7], [6, 8]], 7, 8));
+  });
 });
