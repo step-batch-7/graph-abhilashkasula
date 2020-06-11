977ed136a2308d8fb45e697c44f1fdd8b7b3c32c
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-06-11 07:33:16 +0530

    reverted changes

--- src/graph.js
+++ src/graph.js
@@ -4,30 +4,21 @@
 // To => to
 // Should return true.
 
-const parse = (pairs) => {
-  return pairs.reduce((graph, [key, val]) => {
-    if (key in graph) {
-      graph[key].push(val);
-      return graph;
-    }
-    graph[key] = [val];
-    return graph;
-  }, {});
-};
+const getEdges = (pairs, node) => pairs.filter(pair => pair[0] === node);
 
 const bfs = function (pairs, source, target) {
-  const graph = parse(pairs);
   const visited = [source];
-  const toVisit = graph[source] ? graph[source].slice() : [];
-  while (toVisit.length) {
+  const toVisit = getEdges(pairs, source).map(pair => pair[1]);
+  while (toVisit.length != 0) {
     const current = toVisit.shift();
     visited.push(current);
     if (current === target) return true;
-    const edges = graph[current];
-    edges && edges.forEach(edge =>
-          !visited.includes(edge) &&
-          !toVisit.includes(edge) &&
-          toVisit.push(edge));
+    const sourcePairs = getEdges(pairs, current);
+    sourcePairs.forEach((pair) => {
+      !visited.includes(pair[1]) &&
+        !toVisit.includes(pair[1]) &&
+        toVisit.push(pair[1]);
+    });
   }
   return false;
 };
